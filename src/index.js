665a7e27c3ae771a5a1cbab31d5fbb8f20ed9c31
JustsenSimon <justsensimon@hotmail.com>
2023-06-01 21:34:14 +0200

    Added some new animations

--- src/index.js
+++ src/index.js
@@ -6,27 +6,34 @@ const lastNameWrapper = document.querySelector(".last-name");
 const firstName = "Simon";
 const lastName = "Justsen";
 
-let introText = document.querySelector(".intro-text");
-gsap.from(introText, { opacity: 0, duration: 0.5, delay: 2 });
-
+const introText = document.querySelector(".intro-text");
 const showcaseFrontendWrapper = document.querySelector(
   ".showcase-frontend-technologies"
 );
+const aboutMeWrapper = document.querySelector(".about-me-wrapper");
+const wave = document.querySelector(".wave");
+
+let tlFrontPage = gsap.timeline();
+let tlWave = gsap.timeline();
+tlWave.add(gsap.to(wave, { duration: 0.3, rotateZ: -60 }));
+tlWave.add(gsap.to(wave, { duration: 0.3, rotateZ: -15 }));
+tlWave.repeat(2);
+tlFrontPage.add(gsap.from(introText, { opacity: 0, duration: 0.5, delay: 2 }));
+tlFrontPage.add(
+  gsap.from(aboutMeWrapper, { opacity: 0, y: 40, duration: 0.5 })
+);
+tlFrontPage.add(gsap.from(wave, { opacity: 0, duration: 0.5 }));
+tlFrontPage.add(tlWave);
 
 window.onload = function () {
   const stylesheet = document.styleSheets[0];
 
-  const calculateGridSpacing = (gridWrapper, width, height) => {
+  const calculateGridSpacing = (width) => {
     const iconSize = Math.floor(width / 3 - 30);
     let rule = `.icons { height: ${iconSize}px }`;
     stylesheet.insertRule(rule);
   };
-
-  calculateGridSpacing(
-    showcaseFrontendWrapper,
-    showcaseFrontendWrapper.offsetWidth,
-    showcaseFrontendWrapper.offsetHeight
-  );
+  calculateGridSpacing(showcaseFrontendWrapper.offsetWidth);
 };
 
 const createLetters = (name, wrapper) => {
